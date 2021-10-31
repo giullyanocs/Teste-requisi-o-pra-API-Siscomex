c7a80f10f3be63bbaeaaf2ea3b825b328f4c6410
giullyanocs <giullyanofisica@gmail.com>
2021-10-31 00:51:13 -0300

    segunda atualização

--- API01.py
+++ API01.py
@@ -1,12 +1,12 @@
 """ Este script é um exemplo do uso da biblioteca request(requisições) em um site siscomex,o arquivo requisitado
  é no formato json,um arquivo com funcionalidades parecidas com um dicionário."""
 
-# catalogo de produtos
+# documentos da operação
 
 import requests
+import json
 
-catalogoprod = requests.get("https://val.portalunico.siscomex.gov.br/catp/api/ext/produto")
-catalogoprod = catalogoprod.json()
-catalogo_produto = catalogoprod['info']  # ['visao']
-# print(catalogoprod)
-print(catalogo_produto)
+res = requests.get('https://portalunico.siscomex.gov.br/edocex/api/ext/tipos-documentos-operacao/DI/ext/documentos-dossie/201900012345678')
+print(res)
+#json = res.json()
+#print(json)
